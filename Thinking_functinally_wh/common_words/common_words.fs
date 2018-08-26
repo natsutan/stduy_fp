6c06cf8471dfaeb637396e618fe78e5a0882e981
natsutan <YRW03704@nifty.com>
2018-08-26 20:39:11 +0900

    work

---
+++ Thinking_functinally_wh/common_words/common_words.fs
@@ -0,0 +1,32 @@
+﻿open System
+
+let words = ["To" ;  "be" ; "or " ; "not" ; "to" ; "be"]
+
+let myToLower (s : string) :string = 
+    s.ToLower()
+
+let countRuns (words : string list) : (string * int) list =
+    let mutable result = []
+    let mutable cnt = 1
+    let mutable next = ""
+
+    for ww in  List.windowed 2 words do
+        let w = ww.Item(0)
+        next <- ww.Item(1)
+        if w = next then
+            cnt <- cnt + 1
+        else
+           result <- List.append result [(w, cnt)]
+           cnt <- 1
+    
+    List.append result [(next, cnt)]
+    
+let commonWords (words : string list) (n : int) : (string * int) list =
+    List.map myToLower words
+    |> List.sort
+    |> countRuns 
+    |> List.take n
+
+
+let result = commonWords words 3
+printfn "%A"  result
